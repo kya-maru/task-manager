ace2f3fbcf353852070fefd19c123f0f8e18abd3
maru <138083498+kya-maru@users.noreply.github.com>
2024-06-21 20:56:31 +0900

    Googleでのログイン機能

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAnAJ7WvCzJHt-xSgGSfi5CgG9OfeydbEw",
@@ -13,5 +14,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 export default db;
+export { auth, provider };
